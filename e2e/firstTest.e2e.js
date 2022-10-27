7c4cf8c040afc8327a612f2c3f3c2213c529300c
Ichoku Chinonso <chinonso@mvxchange.com>
2022-10-28 00:42:28 +0100

    cleaning up

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -7,17 +7,12 @@ describe('cryptoMobile', () => {
     await device.reloadReactNative();
   });
 
-  // it('should have crypto tokens screen', async () => {
-  //   await expect(element(by.id('CryptoTokens'))).toBeVisible();
-  // });
+  it('should have crypto tokens screen', async () => {
+    await expect(element(by.id('CryptoTokens'))).toBeVisible();
+  });
 
   it('should be able to search token', async () => {
     await element(by.id('SearchToken')).tap();
     await expect(element(by.text('BUSD'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
   });
 });
